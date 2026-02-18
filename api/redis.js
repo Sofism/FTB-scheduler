304b87a04472acf23d4eb69f635751e6a414b1ab
Sofism <sbatalla1@hotmail.com>
2026-02-18 22:45:00 +0100

    Update redis.js

--- api/redis.js
+++ api/redis.js
@@ -1,13 +1,6 @@
-// api/redis.js
-const { Redis } = require(’@upstash/redis’);
-
-if (!process.env.UPSTASH_REDIS_REST_URL || !process.env.UPSTASH_REDIS_REST_TOKEN) {
-throw new Error(‘Missing UPSTASH_REDIS_REST_URL or UPSTASH_REDIS_REST_TOKEN’);
-}
-
+const { Redis } = require('@upstash/redis');
 const redis = new Redis({
-url: process.env.UPSTASH_REDIS_REST_URL,
-token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
 });
-
 module.exports = redis;
